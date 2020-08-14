8df575ab03ae4b2f542ade64d4ba3b9ac01c6895
Walid-Fahig <walid25397@gmail.com>
2020-08-14 06:06:41 +0200

    add Authentication by Table Admin and Add Table AdminRole And Table Role

---
+++ database/migrations/2020_08_14_034735_create_admin_roles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_roles', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->tinyInteger('role_id')->unsigned();
+            $table->tinyInteger('admin_id')->unsigned();
+            $table->foreign('role_id')
+                ->references('id')->on('admins')
+                ->onDelete('cascade');
+                $table->foreign('admin_id')
+                ->references('id')->on('admins')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_roles');
+    }
+}
